685af8309d2df75ee5b42a4b97fd703bb21d8fde
zhuyuning <zhuyuning_i@didichuxing.com>
2018-11-03 18:57:09 -0700

    3 compponents generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstpartComponent } from './firstpart/firstpart.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstpartComponent,
+    SecondComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/firstpart/firstpart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstpart',
+  templateUrl: './firstpart.component.html',
+  styleUrls: ['./firstpart.component.css']
+})
+export class FirstpartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
