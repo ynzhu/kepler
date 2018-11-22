50b8062772f1cd5d4f6fcd9359a6d82d4b9f5743
TYK <tyk.helloworld@gmail.com>
2018-11-21 23:13:58 -0800

    test map changing

--- src/app/firstpart/firstpart.component.ts
+++ src/app/firstpart/firstpart.component.ts
@@ -60,6 +60,7 @@ export class FirstpartComponent implements OnInit {
           .data(json.features)
           .enter()
           .append("path")
+          .attr("class","states")
           .attr("fill", function (d, i) {
             var index = countryArray.indexOf(d["properties"]["NAME"]);
             if (index >= 0) {
@@ -72,58 +73,6 @@ export class FirstpartComponent implements OnInit {
           .attr("stroke", "black")
           .attr("d", path);
       });
-
-
-      d3.json("src/assets/tradevalue.json").then(function (data: any) {
-        gg.selectAll("circle")
-          .data(data)
-          .enter()
-          .append("circle")
-          .attr("cx", function (d: trade) {
-            return +projection([d.longtitude, d.latitude,])[0]
-          })
-          .attr("cy", function (d: trade) { return +projection([d.longtitude, d.latitude])[1] })
-          .attr("r", function (d: trade) { return +d.trade_2017 / 40; })
-          .attr("fill", "red")
-          .attr("opacity", "0.55");
-
-        gg.selectAll(".first_country")
-          .data(data)
-          .enter()
-          .append("text")
-          .attr("class", "first_country")
-          .attr("x", function (d: trade) {
-            return +projection([d.longtitude, d.latitude,])[0]
-          })
-          .attr("y", function (d: trade) { return +projection([d.longtitude, d.latitude])[1] })
-          .text(function (d: trade) { return d.country });
-
-        svg.append("text")
-          .attr("x", width / 2)
-          .attr("y", 20)
-          .attr("font-size", 20)
-          .attr("font-weight", "bold")
-          .style('text-anchor', 'middle')
-          .text("Proportional Symbol Map for World Satellites")
-        // console.log(d3.extent(data, function(d:trade){return d.trade_2017}))
-        gg.selectAll(".first_legend")
-          .data(d3.extent(data, function (d: trade) { return d.trade_2017 }))
-          .enter()
-          .append("circle")
-          .attr("cx", width - 200)
-          .attr("cy", height - 200)
-          .attr("r", function (d) { return d / 40; })
-          .attr("fill", "red")
-          .attr("opacity", "0.55")
-
-        gg.selectAll(".first_legend")
-          .data(d3.extent(data, function (d: trade) { return d.trade_2017 }))
-          .enter()
-          .append("text")
-          .attr("x", width - 200 + 55)
-          .attr("y", function (d, i) { return height - 200 - i * 20; })
-          .text(function (d) { return Math.round(d / 10) + " Satellites" });
-      })
     });
 
     var width = 960;
@@ -166,14 +115,97 @@ export class FirstpartComponent implements OnInit {
       .call(drag);
 
     function dragmove(d) {
+      function get_year(d) {
+        if (d > 500) {
+          return 2;
+        } else {
+          return 1;
+        }
+      }
       var x = d3.event.x;
       x = x < x1 ? x1 : x > x2 ? x2 : x;
       d.x = x;
       circle.attr("cx", x);
       var x_value_of_circle: any = svg
-      .select('#slider_circle')
-      .attr("cx")
+        .select('#slider_circle')
+        .attr("cx")
+      x_value_of_circle = get_year(x_value_of_circle)
       console.log(x_value_of_circle)
+      var date_now = new Date();
+      var original_time = date_now.getTime();
+      if (x_value_of_circle > 1) {
+        d3.json("src/assets/world_geojson.json").then(function (json: any) {
+          var colorScale = d3.scaleSequential(d3.interpolateOranges).domain([0, 300]);
+          var projection = d3.geoMercator().fitSize([width, height + 100], json);
+          var path = d3.geoPath().projection(projection);
+          d3.json("src/assets/temp_tyk_country_launch_num2.json").then(function (country: any) {
+            var countryArray = new Array();
+            countryArray = country.map(d => d.country);
+            var valueArray = country.map(d => d.num);
+            var jsonValueArray = new Array();
+
+            var jsonCountryArray = new Array();
+            for (var j = 0; j < countryArray.length; j++) {
+              for (var i = 0; i < json.features.length; i++) {
+                if (json.features[i]["properties"]["NAME"] == countryArray[j]) {
+                  jsonCountryArray.push(json.features[i]);
+                  jsonValueArray.push(valueArray[j]);
+                }
+              }
+            }
+
+            gg.selectAll(".states")
+              .data(json.features)
+              .attr("fill", function (d, i) {
+                var index = countryArray.indexOf(d["properties"]["NAME"]);
+                if (index >= 0) {
+                  // console.log(d["properties"]["NAME"] + " " + jsonValueArray[index]);
+                  return (colorScale(jsonValueArray[index]));
+                } else {
+                  return "white";
+                }
+              })
+              .attr("stroke", "black")
+              .attr("d", path);
+          });
+        });
+      } else {
+        d3.json("src/assets/world_geojson.json").then(function (json: any) {
+          var colorScale = d3.scaleSequential(d3.interpolateOranges).domain([0, 300]);
+          var projection = d3.geoMercator().fitSize([width, height + 100], json);
+          var path = d3.geoPath().projection(projection);
+          d3.json("src/assets/temp_tyk_country_launch_num.json").then(function (country: any) {
+            var countryArray = new Array();
+            countryArray = country.map(d => d.country);
+            var valueArray = country.map(d => d.num);
+            var jsonValueArray = new Array();
+
+            var jsonCountryArray = new Array();
+            for (var j = 0; j < countryArray.length; j++) {
+              for (var i = 0; i < json.features.length; i++) {
+                if (json.features[i]["properties"]["NAME"] == countryArray[j]) {
+                  jsonCountryArray.push(json.features[i]);
+                  jsonValueArray.push(valueArray[j]);
+                }
+              }
+            }
+
+            gg.selectAll(".states")
+              .data(json.features)
+              .attr("fill", function (d, i) {
+                var index = countryArray.indexOf(d["properties"]["NAME"]);
+                if (index >= 0) {
+                  // console.log(d["properties"]["NAME"] + " " + jsonValueArray[index]);
+                  return (colorScale(jsonValueArray[index]));
+                } else {
+                  return "white";
+                }
+              })
+              .attr("stroke", "black")
+              .attr("d", path);
+          });
+        });
+      }
     }
   }
 }
